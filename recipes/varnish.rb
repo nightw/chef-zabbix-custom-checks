4462b6c457ab23a8236886f77fe41e6342f58b65
Pal David Gergely <nightw17@gmail.com>
2014-09-17 18:51:11 +0200

    Varnish monitoring: Added Varnish 4.x support using a backward compatible way. Also added a possibility for getting the sum of two stat fields in Varnish 4.x

--- recipes/varnish.rb
+++ recipes/varnish.rb
@@ -9,6 +9,12 @@
 
 include_recipe "zabbix-custom-checks::default"
 
+template "#{node.zabbix.external_dir}/varnish4_stats.sh" do
+	source "varnish/varnish4_stats.sh.erb"
+	mode "755"
+	notifies :restart, "service[zabbix_agentd]", :delayed
+end
+
 template "#{node.zabbix.agent.include_dir}/varnish.conf" do
   source "varnish/zabbix.conf.erb"
   mode "644"
